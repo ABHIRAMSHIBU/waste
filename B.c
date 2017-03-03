84fe60de1519e5f2ca88dafc6f1d854283e1a3a4
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-03-03 12:16:59 +0530

    Update B.c

--- B.c
+++ B.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
     long long int a;
+    clrscr();
     printf("Enter a number :");
     scanf("%lld",&a);
     if(a%2==0)
     {
-        printf("%lld is even !\n",a);
-        main();
+	printf("%lld is even !\n",a);
+      //  main();
     }
     else
     {
-        printf("%lld is odd !\n",a);
-        main();
+	printf("%lld is odd !\n",a);
+       // main();
     }
+    getch();
 }
