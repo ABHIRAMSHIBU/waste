612526f8b6e7fecd8a6b2583c8faefe0227c3f89
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-05-01 21:49:00 +0530

    Added broken multiplication

--- WASTE/Cycle 2/Matrix.c
+++ WASTE/Cycle 2/Matrix.c
@@ -65,7 +65,7 @@ void mat_sum()
 }
 void mat_mult()
 {
-    int *mat1,*mat2,row,col,*matmult,i,j,k;
+    int *mat1,*mat2,row,col,*matmult,i,j,k,count=0;
 	printf("Enter rows and cols rows <space> cols :");
 	scanf("%d %d",&row,&col);
 	mat1=make_mat(row,col);
@@ -74,19 +74,32 @@ void mat_mult()
 	print_mat(mat1,row,col);
 	accept_elements(mat2,row,col);
 	print_mat(mat2,row,col);
+	matmult=make_mat(row,col);
+	k=0;
+	for(i=0;i<row*col;i+row)
+    {
+        for(j=0;j<col;j++)
+        {
+            *(matmult+k)=*(matmult+k)+(*(mat1+i+j)*(*(mat2+(j*row))));
+            k++;
+        }
+    }
+    print_mat(matmult,row,col);
 }
 void main()
 {
-	int *mat,x,y,i,j;
-	printf("Enter the rows and cols :");
-	scanf("%d %d",&x,&y);
-	mat=make_mat(x,y);
-	accept_elements(mat,x,y);
-	while(1)
+    mat_mult();
+	//int *mat,x,y,i,j;
+	//printf("Enter the rows and cols :");
+	//scanf("%d %d",&x,&y);
+	//mat=make_mat(x,y);
+	//accept_elements(mat,x,y);
+
+	/*while(1)
     {
         printf("Enter params :");
         scanf("%d %d",&i,&j);
         print_mat(mat,x,y);
         printf("%d \n",mat[i]);
-    }
+    }*/
 }
