b5d73f8b8a967c833839c0b4c1166eeee9510966
unknown <students@nt139.oslab.edu>
2017-05-05 12:43:42 +0530

    Added today's changes

--- WASTE/Cycle 1/Inerest calc.c
+++ WASTE/Cycle 1/Inerest calc.c
@@ -7,7 +7,7 @@ void main()
  scanf("%f %f", &p,&t);
  if(p<=1000 && t>=2)
   r=0.05;
- else if(p>1000 && p<5000 && t>=2)
+ else if(p>1000 && p<5=000 && t>=2)
   r=0.07;
  else if(p>5000 && t>=1)
   r=0.08;
