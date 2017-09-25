3cc126095b799a6160abd0bed7f71eac2d74bedb
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-09-25 21:28:41 +0530

    rotate_linkedlist.c:Rotation code added!

---
+++ MISC/rotate_linkedlist.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<malloc.h>
+#include "linkedlist.h"
+int rotate(listPointer ** head,int n){
+    listPointer * nodeCurrent= * head, * nodeTemp, * nodeTemp2;
+    if(n==0)
+        return(-2);
+    else if(n<0)
+        return(-1);
+    for(int i=0;i<n-1;i++){
+        if(nodeCurrent->link == NULL || nodeCurrent == NULL)
+            return(-1);
+        nodeCurrent=nodeCurrent->link;
+    }
+    if(nodeCurrent->link==NULL)
+        return(-2);
+    nodeTemp=nodeCurrent->link;
+    nodeCurrent->link=NULL;
+    nodeTemp2=nodeTemp;
+    while(nodeTemp->link!=NULL)
+        nodeTemp=nodeTemp->link;
+    nodeTemp->link=* head;
+    * head = nodeTemp2;
+    return(n);
+}
+int main(){
+    int n;
+    listPointer * head;
+    bulkInsert(&head);
+    printf("Before rotate \n");
+    printAll(&head);
+    printf("Enter where you want to rotate:");
+    scanf("%d",&n);
+    n=rotate(&head,n);
+    if(n==-1)
+        printf("Rotation failed!, may be invaid n\n");
+    else if(n==-2)
+        printf("Rotation not needed or error occured!");
+    else
+        printAll(&head);
+}
