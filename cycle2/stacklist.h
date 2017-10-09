2dbc4c29bffdc7dd242d7b466188078b52dfea68
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-10-09 07:26:48 +0530

    STACK: Upgraded stacklist.h to match linkedlist.h

--- cycle2/stacklist.h
+++ cycle2/stacklist.h
@@ -1,13 +1,13 @@
 #include<malloc.h>
 #include "linkedlist.h"
 void LPUSH(listPointer ** head,int data){
-    insert_front(head,data);
+    insertFront(head,data);
 }
 int LPOP(listPointer ** head){
     if(*head==NULL)
     {
         return -1;
     }
-    return delete_font(head);
+    return deleteFont(head);
 }
 
