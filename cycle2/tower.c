d353e23aef4abd83409d8fb6da8f0cd3cb0ca297
Abhiram Shibu <abhiramshibu1998@gmail.com>
2017-10-27 21:33:19 +0530

    Tower: commit from college

--- cycle2/tower.c
+++ cycle2/tower.c
@@ -1,20 +1,23 @@
+/*Copyright (c) 2017 Abhiram Shibu
+ *Copyright (c) 2017 Team Destroyer Projects\
+ */     
 #include<stdio.h>
 #include "color.h"
 /* Tower of Hanoi recursive way */
 void towerOfHanoi(int * step,int n, char from, char to, char sub)
 {
-	if (n == 1)
-	{
-		printf("\t%sStep:%s%d, ",RED,GRN,* step);
+    if (n == 1)
+    {
+	printf("\t%sStep:%s%d, ",RED,GRN,* step);
         printf("%smove disk %s1",RED,GRN, from, to);
         printf("%s from rod %s%c",RED,GRN, from);
         printf("%s to rod %s%c%s\n",RED,GRN,to,RESET );
         *step=*step+1;
-		return;
-	}
-	towerOfHanoi(step,n-1, from, sub, to);
-	printf("\t%sStep:%s%d, ",RED,GRN,* step);
-    printf("%smove disk %s1",RED,GRN, from, to);
+	return;
+    }
+    towerOfHanoi(step,n-1, from, sub, to);
+    printf("\t%sStep:%s%d, ",RED,GRN,* step);
+    printf("%smove disk %s%d",RED,GRN,n);
     printf("%s from rod %s%c",RED,GRN, from);
     printf("%s to rod %s%c%s\n",RED,GRN,to,RESET );
     *step=*step+1;
