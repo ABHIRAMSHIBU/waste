a6079286d1e46b1ec5763fcfce4deddaaf7ea9ed
Abhiram Shibu <abhiramshibu1998@gmail.com>
2018-02-12 14:23:20 +0530

    Sort by column with input data!

--- cycle1/shellscripts/reverseAName.sh
+++ cycle1/shellscripts/reverseAName.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-printf "Enter a name :"
-read name
-printf "String in reverse :"
-echo $name | rev
-printf "Count of chars including new line :"
-echo $name | wc -c
+printf "Get user name of current user?[Y/y/n/N] :"
+read choice
+if  test $choice = 'y'  ||  test $choice = 'Y'
+then
+	printf "String in reverse :"
+        echo $(whoami) | rev
+        printf "Count of chars including new line :"
+        echo $(whoami) | wc -c
+else
+	printf "Enter a name :"
+	read name
+	printf "String in reverse :"
+	echo $name | rev
+	printf "Count of chars including new line :"
+	echo $name | wc -c
+fi
---
+++ cycle1/shellscripts/sortSchool.datReverse.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Copyleft 2018 Abhiram Shibu
+cat school.dat | sort -k 3nr 
