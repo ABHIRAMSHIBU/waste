a11ebda2ff9bc23e62e18ed9b94ca034ba0b184f
Abhiram Shibu <abhiramshibu1998@gmail.com>
2018-02-12 15:29:05 +0530

    Color header file

---
+++ cycle1/shellscripts/colors.sh
@@ -0,0 +1,10 @@
+YELLOW="\e[33m"
+RED="\e[31m"
+GRAY="\e[90m"
+LIGHT_GRAY="\e[37m"
+CYAN="\e[36m"
+LIGHT_CYAN="\e[96m"
+RESET="\e[0m"
+BLUE="\e[34m"
+GREEN="\e[32m"
+echo -e $GREEN"IMPORTED COLORS!"$NO_COLOUR
