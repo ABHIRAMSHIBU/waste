de08d25d58482750504641eac704f5746a2da80d
Abhiram Shibu <abhiramshibu1998@gmail.com>
2018-02-25 19:56:04 +0530

    Calculator.sh: Finished!

--- cycle2/calculator.sh
+++ cycle2/calculator.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+# Copyleft 2018 Abhiram Shibu
+. colors.sh
 function sum(){
 	if [ $# -eq 2 ]
 	then
@@ -20,21 +23,79 @@ function devide(){
 	if [ $# -eq 2 ]
         then
                 echo $(( $1 / $2 ))
-        fi
+    fi
 }
-function exponent(){
-	if [ $# -eq 2 ]
+function mod(){
+    if [ $# -eq 2 ]
         then
-		exp=$1
-		N=$(( $2 - 1 ))
-                while [ ! $N -eq 0 ]
-        	do
-                	exp=$(( $exp * $2  ))
-                	N=$(( N - 1 ))
-        	done
-		echo $exp
-        fi
+                echo $(( $1 % $2 ))
+    fi
 }
-
-
-echo $(exponent 2 10)
+c=1
+while [ $c -eq 1 ]
+do
+    clear
+    echo -e "$RED---------------------------Main Menu---------------------------------$RESET"
+    echo -e "$GREEN      1) Add"
+    echo "      2) Substract"
+    echo "      3) Multiply"
+    echo "      4) Divide"
+    echo "      5) Mod"
+    echo -e "$RED=====================================================================$RESET"
+    c=0
+    printf "Enter a choice :"
+    read choice
+    if [ $choice -eq 1 ]
+    then
+        printf "Enter first number to add :"
+        read a
+        printf "Enter second number to add :"
+        read b
+        printf "Answer is :"
+        sum $a $b
+    else
+        if [ $choice -eq 2 ]
+        then
+            printf "Enter first number to substract :"
+            read a
+            printf "Enter second number to substract :"
+            read b
+            printf "Answer is :"
+            substract $a $b
+        else
+            if [ $choice -eq 3 ]
+            then
+                printf "Enter first number to multiply :"
+                read a
+                printf "Enter second number to multiply :"
+                read b
+                printf "Answer is :"
+                multiply $a $b
+            else
+                if [ $choice -eq 4 ]
+                then
+                    printf "Enter first number to devide :"
+                    read a
+                    printf "Enter second number to devide :"
+                    read b
+                    printf "Answer is :"
+                    devide $a $b
+                else
+                    if [ $choice -eq 5 ]
+                    then
+                        printf "Enter first number to mod :"
+                        read a
+                        printf "Enter second number to mod :"
+                        read b
+                        printf "Answer is :"
+                        mod $a $b
+                    else
+                        echo -e "$RED Enter a valid input and try again!$RESET"
+                    fi
+                fi
+            fi
+        fi
+    fi
+    printf "Do you want to exit [0-exit, 1-continue]:"
+    read c
+done
