2cf3e88226e22459fef16aa66e0abb545f3611c5
ABHIJITH N RAJ <abhijithnrajg@gmail.com>
2018-10-27 14:50:16 +0530

    Add files via upload

--- JAVA_SQL/EXP2/PLSQL/1.sql
+++ JAVA_SQL/EXP2/PLSQL/1.sql
@@ -1,5 +1,5 @@
 /*
-create table circle1(radius integer primary key ,circumference decimal(20,10),area decimal(20,10));
+create table circle_area1(radius integer primary key ,circumference decimal(20,10),area decimal(20,10));
 */
 
 set serveroutput on;
@@ -18,10 +18,10 @@ BEGIN
 		area:=3.14*r*r;
 		circumference:=2*3.14*r;
 		dbms_output.put_line('		'||r||'		'||circumference||'		'||area);
-		insert into circle1 values(r,circumference,area);
+		insert into circle_area1 values(r,circumference,area);
 	end loop;
 END;
 /
 /*
-delete from circle1;
+delete from circle_area1;
 */
